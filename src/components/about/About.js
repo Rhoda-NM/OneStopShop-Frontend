c6a3d384e724180f340ead94f970d1f53dd91c7d
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-08-06 18:38:37 +0300

    "Added Font Awesome stylesheet, updated About component with stats section and team cards, and modified corresponding CSS styles."

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -26,10 +26,59 @@ const About = () => {
             Launced in 20124, OneStop is a leading onlineshopping platform,, offering a wide range of products for our customers
             One Stop Products has more than 1 Million products to offer, growing at a very fast. One Stop Products offers a diverse assotment in categories ranging  from consumer.
             </p>
-            <button>Read More...</button>
+            {/* <button>Read More...</button> */}
           </div>
         </div>
       </section>
+
+      <section className="stats">
+                <div className="container">
+                    <div className="card">
+                        <div className="face face1">
+                            <div className="content">
+                                <h2>10.5k</h2>
+                                <p>Sellers Active on Our Site</p>
+                            </div>
+                        </div>
+                        <div className="face face2">
+                            <i className="fas fa-users"></i>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="face face1">
+                            <div className="content">
+                                <h2>33k</h2>
+                                <p>Monthly Product Sales</p>
+                            </div>
+                        </div>
+                        <div className="face face2">
+                            <i className="fas fa-box"></i>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="face face1">
+                            <div className="content">
+                                <h2>4.5k</h2>
+                                <p>Customers Active on Our Site</p>
+                            </div>
+                        </div>
+                        <div className="face face2">
+                            <i className="fas fa-user"></i>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="face face1">
+                            <div className="content">
+                                <h2>25k</h2>
+                                <p>Annual Gross Sales</p>
+                            </div>
+                        </div>
+                        <div className="face face2">
+                            <i className="fas fa-dollar-sign"></i>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
       <section className="team">
         <h1>Meet Our Team</h1>
@@ -67,6 +116,51 @@ const About = () => {
           <div className="card">
             <div className="card-img">
               <img 
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20230822183347/man-portrait-businessman-male.jpg" 
+                alt="User 2" 
+              />
+            </div>
+            <div className="card-info">
+              <h2 className="card-name">Miller</h2>
+              <p className="card-role">Co-Founder</p>
+              <p className="card-email">Miller@example.com</p>
+              <p><button className="button">Contact</button></p>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-img">
+              <img 
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20230822183347/man-portrait-businessman-male.jpg" 
+                alt="User 2" 
+              />
+            </div>
+            <div className="card-info">
+              <h2 className="card-name">Miller</h2>
+              <p className="card-role">Co-Founder</p>
+              <p className="card-email">Miller@example.com</p>
+              <p><button className="button">Contact</button></p>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-img">
+              <img 
+                src="https://media.geeksforgeeks.org/wp-content/uploads/20230822183347/man-portrait-businessman-male.jpg" 
+                alt="User 2" 
+              />
+            </div>
+            <div className="card-info">
+              <h2 className="card-name">Miller</h2>
+              <p className="card-role">Co-Founder</p>
+              <p className="card-email">Miller@example.com</p>
+              <p><button className="button">Contact</button></p>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-img">
+              <img 
                 src="https://media.geeksforgeeks.org/wp-content/uploads/20230824122630/business-office-business-woman-professional.jpg" 
                 alt="User 3" 
               />
@@ -79,9 +173,9 @@ const About = () => {
             </div>
           </div>
         </div>
-      </section>
 
-      
+        
+      </section>
     </div>
   );
 };
