66e480085aa225acaf365d026e272da6cc436a6e
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-08-06 20:24:10 +0300

    "Refactored Wishlist component: updated HTML structure, added new CSS styles for cards and buttons, and removed unnecessary code."

--- src/components/wishlist/Wishlist.js
+++ src/components/wishlist/Wishlist.js
@@ -3,141 +3,245 @@ import './wishlist.css';
 
 const WishlistComponent = () => {
   return (
-    // Wishlist
     <div>
-    <section className="wishlist">
-      <h1>Wishlist</h1>
-      <div className="wishlist-cards">
-        <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+      {/* Wishlist */}
+      <section className="wishlist">
+        <h1>Wishlist</h1>
+        <div className="wishlist-cards">
+          {/* Card 1 */}
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
           </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 1</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
           </div>
-        </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 1</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div> <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 1</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div> <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 1</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 2" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 2</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 3" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 3</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
           </div>
           
+          {/* Repeat for other cards */}
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
+          </div>
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    
-    {/* // Just For You */}
-    <section className="wishlist">
-      <h1>Just For You</h1>
-      <div className="wishlist-cards">
-        <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+      {/* Just For You */}
+      <section className="wishlist">
+        <h1>Just For You</h1>
+        <div className="wishlist-cards">
+          {/* Repeat the same card structure for Just For You section */}
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
           </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 1</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
+          </div>
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
+          </div>
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
+          </div>
+          <div className="card-wrapper">
+            <div className="card">
+              <div className="card-img">
+                <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
+              </div>
+              <button className="card-btn">
+                <i className="fas fa-cart-plus"></i> Add to Cart
+              </button>
+            </div>
+            <div className="card-details">
+              <h2 className="card-name">Item 1</h2>
+              <div className="rating">
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star"></i>
+                <i className="fas fa-star-half-alt"></i>
+                <i className="far fa-star"></i>
+              </div>
+              <p className="card-description">Description for Item 1.</p>
+              <p className="card-price">$19.99</p>
+            </div>
           </div>
         </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 2" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 2</h2>
-            <p className="card-description">item 2.</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 3" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 3</h2>
-            <p className="card-description">Description .</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 1</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div> <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 1</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div> <div className="card">
-          <div className="card-img">
-            <img src="https://i.pinimg.com/474x/07/b2/f1/07b2f18b6e6e93fe26486d275d547b08.jpg" alt="Item 1" />
-          </div>
-          <div className="card-info">
-            <h2 className="card-name">Item 1</h2>
-            <p className="card-description">Description.</p>
-            <button className="card-btn">Remove</button>
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
     </div>
-    
-
   );
 };
 
