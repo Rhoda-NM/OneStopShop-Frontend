b97b043cf94a066bf2fb3d0ec3b4d9e3fc82213d
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-08-06 20:42:34 +0300

    "Swap WishlistComponent and About imports and usage in App.js"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import About from './components/about/About';
-// import WishlistComponent from './components/wishlist/Wishlist';
+// import About from './components/about/About';
+import WishlistComponent from './components/wishlist/Wishlist';
 
 const App = () => {
-//   return (
-//     <div className="App">
-//       <WishlistComponent />
-//     </div>
-//   );
-// };
-return (
-  <div className="App">
-    <About />
-  </div>
-);
+  return (
+    <div className="App">
+      <WishlistComponent />
+    </div>
+  );
 };
+
+
+// return (
+//   <div className="App">
+//     <About />
+//   </div>
+// );
+// };
 
 export default App;
