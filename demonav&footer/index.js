5cccf6cfa139f1e315c623c800dc4db54b06e57d
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-08-06 09:00:30 +0300

    added logo svg and reponsiveness upto 320px

--- demonav&footer/index.js
+++ demonav&footer/index.js
@@ -1,12 +1,16 @@
 const navMenu =document.getElementById('nav-menu'),
       navToggle = document.getElementById('toggle-btn'),
       navClose = document.getElementById('nav-close'),
-      Search_area = document.getElementById('Search-area')
+      Search_area = document.getElementById('Search-area'),
+      toggle = document.getElementById('toggle-btn')
+
+
 
 if(navToggle){
     navToggle.addEventListener('click', () =>{
     navMenu.classList.add('show-menu')
     Search_area.classList.add('show-form')
+    toggle.classList.add('active')
     })
 }
      
@@ -15,5 +19,6 @@ if(navClose){
     navClose.addEventListener('click', () =>{
      navMenu.classList.remove('show-menu')
      Search_area.classList.remove('show-form')
+     toggle.classList.remove('active')
     })
     }
