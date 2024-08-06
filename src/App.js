591fdc030685798db8f4ec3e74325859b3510d3a
Maria Kamau <maria.kamau@student.moringaschool.com>
2024-08-06 20:38:39 +0300

    "Updated App.js to render About component instead of WishlistComponent, and modified about.css to adjust team section styling"

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import WishlistComponent from './components/wishlist/Wishlist';
+import About from './components/about/About';
+// import WishlistComponent from './components/wishlist/Wishlist';
 
 const App = () => {
-  return (
-    <div className="App">
-      <WishlistComponent />
-    </div>
-  );
+//   return (
+//     <div className="App">
+//       <WishlistComponent />
+//     </div>
+//   );
+// };
+return (
+  <div className="App">
+    <About />
+  </div>
+);
 };
 
 export default App;
