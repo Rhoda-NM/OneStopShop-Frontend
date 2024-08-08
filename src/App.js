b3ff39ce795484fcc5ccd57ee02faf6a656c53bf
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-08-08 12:39:35 +0300

    Configured user signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-// import About from './components/about/About';
-import WishlistComponent from './components/wishlist/Wishlist';
-
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from './AuthProvider'
+//import About from './components/about/About';
+//import WishlistComponent from './components/wishlist/Wishlist';
+//import Home from './components/Home/Home';
+//import ContactForm from './components/contact/ContactForm';
+//import ProductDetails from './components/products/ProductDetails';
+//import Billings from './components/billing/Billings';
+import SignUpPage from './components/user/SignUpPage';
 const App = () => {
   return (
-    <div className="App">
-      <WishlistComponent />
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <SignUpPage />
+      </div>
+    </AuthProvider>
+    
   );
 };
 
