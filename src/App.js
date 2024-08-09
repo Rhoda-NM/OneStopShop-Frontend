caa65c676f51dde9ecb7c8192b3f42be2c7ba5c9
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-08-09 11:39:04 +0300

    App.js to render  home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 // import About from './components/about/About';
 import WishlistComponent from './components/wishlist/Wishlist';
+import LoginPage from './components/user/LogInPage';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Billings from './components/billing/Billings';
+import Home from './components/Home/Home';
 
 const App = () => {
   return (
     <div className="App">
-      <WishlistComponent />
+      <Home />
     </div>
   );
 };
