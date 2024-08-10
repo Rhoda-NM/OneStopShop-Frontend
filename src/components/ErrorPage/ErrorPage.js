10517c46ce90e3bdeda820622260a9a64308f1ef
bravinkibetnyangusei <bravink599@gmail.com>
2024-08-10 16:44:44 +0300

    updated error.css

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -3,22 +3,22 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './ErrorPage.css';
 
-function ErrorPage() {
-    return (
-        <>
-            <Header />
-            <hr className="separator"/>
-            <div className="error-page">
-                <div className="error-container">
-                    <p className="breadcrumb">Home / 404 Error</p>
-                    <h1 className="error-title">404 Not Found</h1>
-                    <p className="error-message">Page not found. Return to home page.</p>
-                    <a href="/" className="back-home-btn">Back to home page</a>
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
-}
+const ErrorPage = () => {
+  return (
+    <>
+      <Header />
+      <hr className="separator" />
+      <div className="error-page">
+        <div className="error-container">
+          <p className="breadcrumb">Home / 404 Error</p>
+          <h1 className="error-title">404 Not Found</h1>
+          <p className="error-message">Page not found. Return to the home page.</p>
+          <a href="/" className="back-home-btn">Back to home page</a>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default ErrorPage;
