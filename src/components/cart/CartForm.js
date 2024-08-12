895568f65a47bb8412237a9d7260c7099a57984b
Lennis <lennismaina75@gmail.com>
2024-08-12 10:14:26 +0300

    Added Search

--- src/components/cart/CartForm.js
+++ src/components/cart/CartForm.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './CartForm.css';
 
 const CartForm = () => {
   const [cartItems, setCartItems] = useState([]);
