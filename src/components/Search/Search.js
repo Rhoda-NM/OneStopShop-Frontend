261eb257749acd7ec0cb126bd1ab0b392ab90901
Lennis <lennismaina75@gmail.com>
2024-08-12 10:28:42 +0300

    Added Search

---
+++ src/components/Search/Search.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const Search = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    onSearch(query);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        className='search-bar'
+        placeholder='What are you looking for?'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default Search;
